c378395d56d8ebbb05bd11f80bbd83d38fb64a8d
CPMAizal <cpmayyyan@gmail.com>
2024-10-11 18:07:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from rich.text import Text
 from rich.style import Style
 from carparktool import CarParkTool
 
-__CHANNEL_USERNAME__ = "CPMVoon"
-__GROUP_USERNAME__   = "CPMVoonChat"
+__CHANNEL_USERNAME__ = "gala_cpm"
+__Owner_USERNAME__   = "gala_original"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -36,7 +36,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name = figlet_format('Voon11', font='bloody')
+    brand_name = figlet_format('CPMGala', font='bloody')
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
@@ -44,7 +44,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text, end=None)
-    console.print("[bold green]♕ CPMVoon11 voo22[/bold green]: شیئرنگ رسائی کی کلید ممنوع ہے اور اسے بلاک کر دیا جائے گا۔.")
+    console.print("[bold green]♕ CPMGala[/bold green]: شیئرنگ رسائی کی کلید ممنوع ہے اور اسے بلاک کر دیا جائے گا۔.")
     console.print(f"[bold green]♕ Telegram[/bold green]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue] or [bold blue]@{__GROUP_USERNAME__}[/bold blue].")
     console.print("[bold red]==================================================[/bold red]")
     console.print("[bold yellow]! Note[/bold yellow]: اپنے سی پی ایم کے اکاؤنٹ کو لاگ آؤٹ کرنے کے لیے برا استمال ہو گا۔ !.", end="\n\n")
